733af1ae77b306abbb3e08f38f98a1ccea721422
MajorTwip <majortwip@twipnet.ch>
2015-07-29 10:26:36 +0200

    Update ESH2Dashing.rb
    Modified header

--- ESH2Dashing.rb
+++ ESH2Dashing.rb
@@ -1,3 +1,8 @@
+#!/usr/bin/ruby -w
+
+# This is a quick and dirty solution to update Tiles in Dashing when a state in Openhab2 / ESH gets updated
+# Attention, I use altered tiles
+# Author: MajorTwip (majortwip(at)twipnet.ch)
 require 'em-eventsource'
 require 'json'
 
@@ -8,19 +13,22 @@ Debuglevel = 0
 
 def Openhabevent(item,state)
     puts "Function for Item #{item} started, value #{state}"
+    # in this example, when the item "Test" receives an update, the data to send can be altered
     case item
     when "Test"
       send_event("welcome", "text", state)
     else
+    # if nothing to do, then the Tile "itemname" will get un update for his data-state to "state"
       send_event(item, "state", state)
     end    #code
 end
 
 def send_event(widget,content,state)
+    #to do: use YOUR_AUTH_TOKEN-variable to change on top of the script
     dashinghash = { auth_token: "YOUR_AUTH_TOKEN"}
     dashinghash[content] = state
     http = EventMachine::HttpRequest.new("http://localhost:3030/widgets/#{widget}").post :body => dashinghash.to_json
-    http.errback { p 'Uh oh'}
+    http.errback { p 'error while sending to Dashing'}
     http.callback {
         if Debuglevel == 1
           p http.response_header.status
@@ -28,7 +36,7 @@ def send_event(widget,content,state)
           p http.response
         end
     }
-    puts "sent!!"
+    puts "update sent!!"
 end
 
 EM.run do
@@ -45,7 +53,7 @@ EM.run do
   end
   source.error do |error|
     puts "error #{error}"
-    sleep(30)
+    sleep(60)
     source.close
     source.start
   end
